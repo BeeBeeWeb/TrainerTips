4e5b606b5ba7e7b9d3663669790f7053e9153257
bhnbhushan <babar.bhushan@gmail.com>
2017-01-15 00:35:35 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+//var colWidth = $('.get-width').outerWidth();
+    // alert(imageHeight);
+    //$('.set-width').css('background-position',-colWidth,100%);
+
+
+// $(document).ready(function() {
+
+//     // var colWidth = $('.get-width1').outerWidth();
+//     // alert(colWidth);
+
+
+//     function myFunction() {
+
+//     // var colWidth = $('.get-width1').outerWidth();
+//     // alert(colWidth);
+
+
+//     $(document).getElementById("set-position2").style.backgroundPosition = "-(230)px 0px";
+// }
+
+// });
+
+
+'use strict';
+
+$(document).ready(function() {
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position2").style.backgroundPosition = -colWidth1+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position3").style.backgroundPosition = -(colWidth1*2)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position4").style.backgroundPosition = -(colWidth1*3)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position5").style.backgroundPosition = -(colWidth1*4)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position6").style.backgroundPosition = -(colWidth1*5)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position7").style.backgroundPosition = -(colWidth1*6)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position8").style.backgroundPosition = -(colWidth1*7)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position9").style.backgroundPosition = -(colWidth1*8)+'px '+'center';
+
+    var colWidth1 = $('.get-width1').outerWidth();
+    document.getElementById("set-position10").style.backgroundPosition = -(colWidth1*9)+'px '+'center';
+
+
+
+
+
+
+
+
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position12").style.backgroundPosition = -colWidth1+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position13").style.backgroundPosition = -(colWidth1*2)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position14").style.backgroundPosition = -(colWidth1*3)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position15").style.backgroundPosition = -(colWidth1*4)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position16").style.backgroundPosition = -(colWidth1*5)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position17").style.backgroundPosition = -(colWidth1*6)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position18").style.backgroundPosition = -(colWidth1*7)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position19").style.backgroundPosition = -(colWidth1*8)+'px '+'center';
+
+    var colWidth1 = $('.get-width11').outerWidth();
+    document.getElementById("set-position20").style.backgroundPosition = -(colWidth1*9)+'px '+'center';
+
+
+});
